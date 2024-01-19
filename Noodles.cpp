f5e16e66b29ad92d8ad26ef9ff117c6683b35969
Orgonah <yasinyasin.1381@gmail.com>
2024-01-19 21:44:28 +0330

    add file

---
+++ Noodles.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //1l1l1l1 ll1l1l1 l11ll11 ll1l1l1 l1l11l1 l1l1l11 l11l11l
+#define l1l1lll +   //l1lll11 l1lll11 1l1l1l1 l1l1l1l 11l1l1l l1l11l1 11ll1l1
+#define l1ll1ll *   //1l1l1l1 ll1l1l1 l11ll11 ll1l1l1 l1l11l1 l1l1l11 l11l11l
+#define l1lll1l =   //l1l1l1l 1l1ll1l 1l1l11l l11ll1l 1l1l1l1 1l1l1l1 l11l11l
+#define ll1ll1l ++  //l1l111l 1l1l1ll 1l1l1l1 1ll11ll 1l1l1l1 l11l1l1 l1l11l1
+#define l11l1ll *=  //11l1l1l ll1l1l1 11ll11l 1l1l1l1 11l1l1l l11l11l 1l1ll1l
+#define l1l1l1l -   //1l1l1l1 l1l1l1l 1ll11ll l1l1l1l 1l1l1l1 11l11l1 l1ll1l1
+#define ll11lll )   //l1l1l1l 11l1l1l 1ll1l1l 1l11ll1 1ll1l1l l1l11l1 l1l1l11
+#define lll1l11 (   //l1l1ll1 l1l1l11 ll11ll1 l1l1l1l 11l1l1l 1l11l11 l11l1ll
+#define l1ll1l1 goto l1lll11//1ll1111 1l1l1l1 ll1l1l1 l11ll11 ll1l1l1 l1l11l1
+#define ll1l1ll -=  //11l1l1l ll1l1l1 11ll11l 1l1l1l1 11l1l1l l11l11l 1l1ll1l
+#define lll11ll %   //l1lll11 l1lll11 1l1l1l1 l1l1l1l 11l1l1l l1l11l1 11ll1l1
+#define ll1l1l1 :   //1l1l1l1 l1l1l1l 1ll11ll l1l1l1l 1l1l1l1 11l11l1 l1ll1l1
+#define l11llll if   lll1l11//l1l111l 1l1l1ll 1l1l1l1 1ll11ll 1l1l1l1 l11l1l1
+int main(){int lll11l1 l1lll1l l1l1l1l 11,l11lll1 l1lll1l 1ll,lll1ll1 l1lll1l
+11l,ll1lll1 l1lll1l l1l1l1l 111l,l1l1ll1 l1lll1l 1111ll,l1l11ll l1lll1l 111l;
+	  ll1lll1 ll1l1ll l1l1l1l ll1ll1l l11lll1 l1ll1ll lll1l11 lll1ll1 l11l1ll 
+	  l11lll1 ll1ll1l l1l1l1l lll11l1 ll11lll;l1lll11 ll1l1l1 l1l11ll ll1l1ll 
+	  l11lll1 ll1l1ll ll1lll1 lll11ll lll1ll1?l1l1ll1 ll1l1ll l1l1l1l ll1lll1 
+      l1l1l1l l11lll1 ll1l1l1 ll1lll1 l1l1l1l l11lll1;l11llll lll11l1 ll1ll1l
+ll11lll{ll1lll1 ll1l1ll l1l1ll1;l1ll1l1;}std::cout<<l1l11ll lll11ll lll1ll1;}
